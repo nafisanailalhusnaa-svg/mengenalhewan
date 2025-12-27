fec76ac8a77fafdbfeeb7d8187172cbaf9e213ce
nafisanailalhusnaa-svg <nafisanailalhusnaa@gmail.com>
2025-12-27 11:40:37 +0700

    file upload

---
+++ story_content/user.js
@@ -0,0 +1,20 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wP1L3YHHhZ":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+
+
+}
+
